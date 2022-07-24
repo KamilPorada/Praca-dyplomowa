34acba17749eea8606f9760b246344cdbb40d707
Kamil0k <kamil.porada@onet.pl>
2022-07-24 21:56:39 +0200

    Modyfikacja layoutu Moje gospodarstwo.

--- app/src/main/java/FragmentsClass/BottomFragments/FarmFragment.java
+++ app/src/main/java/FragmentsClass/BottomFragments/FarmFragment.java
@@ -4,19 +4,16 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,27 +21,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.pracadyplomowa.R;
+import com.google.android.material.textfield.TextInputEditText;
 
-import DataBase.DataBaseHelper;
-import DataBase.DataBaseNames;
+import java.util.Objects;
+
 import HelperClasses.InformationDialog;
-import HelperClasses.ShowToast;
 import HelperClasses.StatisticsHelper;
 import HelperClasses.ToolClass;
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 
 public class FarmFragment extends Fragment {
 
     private Context context;
-
-    private TextView howName, howHighgroves, howField, howMoney;
-    private EditText editName, editHighgroves, editField;
-    private Button cancelButton, acceptButton;
+    private TextView howOwner, howHighgroves, howField, howMoney, howOutgoing;
+    private ImageView editOwnerButton, editHighgrovesButton, editFieldButton;
 
     @Nullable
     @Override
@@ -53,6 +42,7 @@ public class FarmFragment extends Fragment {
         context = container.getContext();
         View view=inflater.inflate(R.layout.layout_farm_fragment,container,false);
         findViews(view);
+        createListeners();
         loadData();
         return view;
     }
@@ -63,114 +53,117 @@ public class FarmFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.tollbar_menu_farm, menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id=item.getItemId();
-        switch (id)
-        {
-            case R.id.information:
-            {
-                InformationDialog informationDialog = new InformationDialog();
-                informationDialog.openInformationDialog(context,getResources().getString(R.string.describes_farm));
-            }break;
-            case R.id.change_farm_data:
-            {
-                openChangeFarmDataDialog();
-            }break;
+        if (id == R.id.information) {
+            InformationDialog informationDialog = new InformationDialog();
+            informationDialog.openInformationDialog(context, getResources().getString(R.string.describes_farm));
         }
         return super.onOptionsItemSelected(item);
     }
 
     private void findViews(View view) {
-        howName=view.findViewById(R.id.how_name);
+        howOwner=view.findViewById(R.id.how_owner);
         howHighgroves=view.findViewById(R.id.how_highgroves);
         howField=view.findViewById(R.id.how_field);
         howMoney=view.findViewById(R.id.how_money);
+        howOutgoing=view.findViewById(R.id.how_outgoing);
+        editOwnerButton=view.findViewById(R.id.edit_owner_button);
+        editHighgrovesButton=view.findViewById(R.id.edit_highgroves_button);
+        editFieldButton=view.findViewById(R.id.edit_field_button);
+    }
+
+    private void createListeners() {
+        View.OnClickListener listener = v -> {
+            int id = v.getId();
+            openChangeFarmDataDialog(id);
+        };
+        editOwnerButton.setOnClickListener(listener);
+        editHighgrovesButton.setOnClickListener(listener);
+        editFieldButton.setOnClickListener(listener);
     }
 
     @SuppressLint("SetTextI18n")
     private void loadData() {
         SharedPreferences sharedPreferences = context.getSharedPreferences("FARM_DATA",Context.MODE_PRIVATE);
-        String name = sharedPreferences.getString("NAME", "");
-        String highgroves = sharedPreferences.getString("HIGHGROVES",howHighgroves.getText().toString());
-        String field = sharedPreferences.getString("FIELD", howField.getText()+" ha");
-
-        howMoney.setText(Math.round(StatisticsHelper.calculateIncomeFromHighgrove(context, ToolClass.getActualYear(), 1) * 100.0) / 100.0 + " zł");
-        howName.setText("Właściciel gospodarstwa\n"+name);
+        String owner = sharedPreferences.getString("NAME", "");
+        String highgroves = sharedPreferences.getString("HIGHGROVES","");
+        String field = sharedPreferences.getString("FIELD", "") + " ha";
+        @SuppressLint("DefaultLocale") String money = String.format("%.2f", Math.round(StatisticsHelper.calculateIncomeFromHighgrove(context,ToolClass.getActualYear(),1) * 100.0) / 100.0) + " zł";
+        String outgoing = StatisticsHelper.getMoneyFromOutgoings(context,ToolClass.getActualYear()) + "zł";
+        howOwner.setText(owner);
         howHighgroves.setText(highgroves);
-        howField.setText(field+" ha");
+        howField.setText(field);
+        howMoney.setText(money);
+        howOutgoing.setText(outgoing);
     }
 
-    private void openChangeFarmDataDialog() {
+    private void openChangeFarmDataDialog(int id) {
         Dialog changeDataDialog = new Dialog(context);
         changeDataDialog.getWindow().setLayout(WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.WRAP_CONTENT);
         changeDataDialog.setContentView(R.layout.dialog_change_farm_data);
         changeDataDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        changeDataDialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
         changeDataDialog.show();
-        findChangeSeasonDialogViews(changeDataDialog);
-        createAndAddListeners(changeDataDialog);
+        createAndAddListeners(changeDataDialog, id);
     }
 
-    private void findChangeSeasonDialogViews(Dialog changeDataDialog) {
-        editName=changeDataDialog.findViewById(R.id.edit_name);
-        editHighgroves=changeDataDialog.findViewById(R.id.edit_highgroves);
-        editField=changeDataDialog.findViewById(R.id.edit_field);
-        cancelButton=changeDataDialog.findViewById(R.id.cancel_button);
-        acceptButton=changeDataDialog.findViewById(R.id.accept_button);
-    }
+    @SuppressLint("NonConstantResourceId")
+    private void createAndAddListeners(Dialog changeDataDialog, int id) {
+        ImageView image = changeDataDialog.findViewById(R.id.image);
+        TextView title = changeDataDialog.findViewById(R.id.title);
+        TextInputEditText howChangeValue = changeDataDialog.findViewById(R.id.how_change_value);
+        Button acceptButton = changeDataDialog.findViewById(R.id.accept_button);
+        Button cancelButton = changeDataDialog.findViewById(R.id.cancel_button);
 
-    private void createAndAddListeners(Dialog changeDataDialog) {
-        @SuppressLint("NonConstantResourceId") View.OnClickListener listener = v -> {
-            int id=v.getId();
-            switch (id)
-            {
-                case R.id.accept_button:
-                {
-                    validateData();
-                    changeDataDialog.dismiss();
-                }break;
-                case R.id.cancel_button:
-                {
-                    changeDataDialog.cancel();
-                }
-            }
-        };
-        acceptButton.setOnClickListener(listener);
-        cancelButton.setOnClickListener(listener);
-    }
-
-    private void validateData() {
-        ShowToast showToast = new ShowToast();
-        if (String.valueOf(editName.getText()).compareTo("")==0 ||
-            String.valueOf(editHighgroves.getText()).compareTo("")==0 ||
-            String.valueOf(editField.getText()).compareTo("")==0)
-                showToast.showErrorToast(context,"BŁĄD EDYCJI!\nUzupełnij wszystkie pola!", R.drawable.icon_information);
-        else
-            editDataFarm();
-    }
-
-    @SuppressLint("SetTextI18n")
-    private void editDataFarm() {
         SharedPreferences sharedPreferences = context.getSharedPreferences("FARM_DATA",Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        editor.putString("NAME", editName.getText().toString());
-        editor.putString("HIGHGROVES", editHighgroves.getText().toString());
-        editor.putString("FIELD", editField.getText().toString());
-        editor.apply();
 
-        howName.setText("Właściciel gospodarstwa\n"+editName.getText());
-        howHighgroves.setText(editHighgroves.getText());
-        howField.setText(editField.getText()+" ha");
+        switch (id) {
+            case R.id.edit_owner_button: {
+                image.setImageResource(R.drawable.image_owner);
+                title.setText("Edycja właściciela\ngospodarstwa");
+                howChangeValue.setHint("[Imię i nazwisko]");
+                howChangeValue.setInputType(0x00000001);
+            }break;
+            case R.id.edit_highgroves_button: {
+                image.setImageResource(R.drawable.image_highgrove);
+                title.setText("Edycja ilości\ntuneli");
+                howChangeValue.setHint("[Ilość sztuk]");
+                howChangeValue.setInputType(0x00000002);
+            }break;
+            case R.id.edit_field_button: {
+                image.setImageResource(R.drawable.image_area_answer);
+                title.setText("Edycja powierzchni\ngospodarstwa");
+                howChangeValue.setHint("[ha]");
+                howChangeValue.setInputType(0x00002002);
+            }break;
+        }
+
+        acceptButton.setOnClickListener(v -> {
+            if (!(Objects.requireNonNull(howChangeValue.getText()).toString().compareTo("") == 0)) {
+                switch (id) {
+                    case R.id.edit_owner_button: {
+                        editor.putString("NAME", howChangeValue.getText().toString());
+                    }
+                    break;
+                    case R.id.edit_highgroves_button: {
+                        editor.putString("HIGHGROVES", howChangeValue.getText().toString());
+                    }
+                    break;
+                    case R.id.edit_field_button: {
+                        editor.putString("FIELD", howChangeValue.getText().toString());
+                    }
+                    break;
+                }
+                editor.apply();
+                changeDataDialog.dismiss();
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FarmFragment()).commit();
+            }
+        });
+
+        cancelButton.setOnClickListener(v -> changeDataDialog.dismiss());
     }
-
-
 }
